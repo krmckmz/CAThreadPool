62bdde5ffaa14a6b064bc6ed12f74a786208c318
Kerem Cokmez <kerem.cokmez@medyasoft.com.tr>
2022-04-24 15:13:18 +0300

    Add project files.

---
+++ CAThreadPool/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        ThreadPool.QueueUserWorkItem(ThreadProc);
+        Console.WriteLine("Main thread does work then sleeps");
+        Thread.Sleep(1000);
+        var isThreadPoolThread = Thread.CurrentThread.IsThreadPoolThread;
+        Console.WriteLine(isThreadPoolThread);
+
+        Console.WriteLine("Main thread exists");
+    }
+    static void ThreadProc(Object stateInfo)
+    {
+        var isThreadPoolThread = Thread.CurrentThread.IsThreadPoolThread;
+        Console.WriteLine(isThreadPoolThread);
+        Console.WriteLine("Hello from thread pool");
+    }
+}
